f54524e5fc2f9e365d1b0d98232c9919d93c5699
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-27 23:09:46 -0600

    added password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-     @user = User.new(name: "Example User", email: "user@example.com")
-   end
+    @user = User.new(name: "Example User", email: "user@example.com",
+                         password: "foobar", password_confirmation: "foobar")
+  end
 
    test "should be valid" do
      assert @user.valid?
@@ -41,4 +42,12 @@ class UserTest < ActiveSupport::TestCase
      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
    end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
