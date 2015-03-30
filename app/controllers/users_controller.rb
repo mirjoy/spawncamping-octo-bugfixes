a83c49f463b6a059b70c49131488d52a76bd24c2
Miriam Moser <mirjoy.moser@gmail.com>
2015-03-29 22:08:24 -0600

    added mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
